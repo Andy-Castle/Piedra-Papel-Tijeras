664868422bdeb69304145835284697871e6c5b7b
Andy-Castle <masterchief_and@live.com>
2024-05-02 21:45:44 -0500

    ya me canse tengo que ver como arreglo esto, todavia debo mejorar

--- script.js
+++ script.js
@@ -1,69 +1,86 @@
 let humanScore = 0;
 let ComputerScore = 0;
 let totalRound = 5;
+let arrayUserSelections = [];
+let arrayComputerChoice = [];
 
-// function getComputerChoice() {
-//   let randomNumber = Math.floor(Math.random() * 3) + 1;
-//   if (randomNumber == 1) {
-//     return "rock";
-//   } else if (randomNumber == 2) {
-//     return "paper";
-//   } else {
-//     return "sycissors";
-//   }
-// }
+const buttonChoices = document.querySelectorAll("button");
 
-// function getUserChoice() {
-//   let userChoice = parseInt(
-//     prompt("Choice 1 for ROCK, 2 for PAPER or 3 for SYCISSORS")
-//   );
-//   if (userChoice == 1) {
-//     return "rock";
-//   } else if (userChoice == 2) {
-//     return "paper";
-//   } else {
-//     return "sycissors";
-//   }
-// }
+const userSelections = document.querySelector("#userSelections");
+const results = document.querySelector("#results");
 
-// function playRound(humanChoice, computerChoice) {
-//   if (humanChoice === computerChoice) {
-//     console.log("TIE");
-//     return "TIE";
-//   } else if (humanChoice == "rock" && computerChoice == "paper") {
-//     humanScore++;
-//     console.log("You win");
-//     return "You win";
-//   } else if (humanChoice == "paper" && computerChoice == "rock") {
-//     humanScore++;
-//     console.log("You win");
-//     return "You win";
-//   } else if (humanChoice == "sycissors" && computerChoice == "paper") {
-//     humanScore++;
-//     console.log("You win");
-//     return "You win";
-//   } else {
-//     ComputerScore++;
-//     console.log("Computer win");
-//     return "Computer win";
-//   }
-// }
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber == 1) {
+    arrayComputerChoice.push("rock");
+    return "rock";
+  } else if (randomNumber == 2) {
+    arrayComputerChoice.push("paper");
+    return "paper";
+  } else {
+    arrayComputerChoice.push("scissors");
+    return "scissors";
+  }
+}
 
-// function playGame() {
-//   const humanSelection = getUserChoice();
-//   const computerSelection = getComputerChoice();
-//   playRound(humanSelection, computerSelection);
-// }
+function getUserChoice() {
+  buttonChoices.forEach((select) => {
+    select.addEventListener("click", () => {
+      if (select.id == "rock") {
+        arrayUserSelections.push("rock");
+        userSelections.textContent = arrayUserSelections;
+        alert("Rock selected");
+        return "rock";
+      } else if (select.id == "paper") {
+        arrayUserSelections.push("paper");
+        userSelections.textContent = arrayUserSelections;
+        alert("Paper selected");
+        return "paper";
+      } else {
+        arrayUserSelections.push("scissors");
+        userSelections.textContent = arrayUserSelections;
+        alert("Scissors selected");
+        return "scissors";
+      }
+    });
+  });
+}
 
-// function totalScore() {
-//   console.log("User Score: " + humanScore);
-//   console.log("Computer Score: " + ComputerScore);
-// }
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "TIE";
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    humanScore++;
+    return "You win";
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    humanScore++;
+    return "You win";
+  } else if (humanChoice == "sycissors" && computerChoice == "paper") {
+    humanScore++;
+    return "You win";
+  } else {
+    ComputerScore + 1;
+    return "Computer win";
+  }
+}
 
+function playGame() {
+  const humanSelection = getUserChoice();
+
+  const computerSelection = getComputerChoice();
+  playRound(humanSelection, computerSelection);
+}
+
+function totalScore() {
+  results.innerHTML = `<p>User Score: ${humanScore}</p>\n
+  <p>Computer Score: ${ComputerScore}</p>`;
+}
+
+playGame();
 // for (let i = 1; i <= totalRound; i++) {
 //   playGame();
 // }
 
-// if (totalRound == 5) {
-//   totalScore();
-// }
+if (totalRound == 5) {
+  totalScore();
+}
